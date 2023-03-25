32d8ec6c36020a8d4113a536949c6e85d32a30b9
Levy Nunes <levy.vix@gmail.com>
2023-03-25 18:15:39 -0300

    add gender

--- insert_to_database.py
+++ insert_to_database.py
@@ -1,9 +1,9 @@
 import datetime
 import json
 
-from sqlalchemy import Boolean, Column, Date, Float, Integer, String, create_engine
+from sqlalchemy import Boolean, Column, Date, Float, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, relationship
 
 Base = declarative_base()
 
@@ -16,7 +16,7 @@ class Movie(Base):
     titulo_original = Column(String)
     imdb = Column(Float)
     ano = Column(Integer)
-    genero = Column(String)
+    # genero = Column(String)
     tamanho_mb = Column(Float)
     duracao_minutos = Column(Integer)
     qualidade = Column(Float)
@@ -24,6 +24,13 @@ class Movie(Base):
     sinopse = Column(String)
     date_updated = Column(Date)
     link = Column(String)
+
+class Gender(Base):
+
+    __tablename__ = "genders"
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    movie_id = Column(Integer, ForeignKey('movies.id'))
+    gender = Column(String)
 
 
 # insert data into the database, if the data already exists, ignore it
@@ -45,19 +52,46 @@ def insert_to_database(json_path, engine):
             data = json.load(json_file)
 
         for movie in data:
-            movie = Movie(**movie)
+            movie_entity = Movie(
+                titulo_dublado=movie["titulo_dublado"],
+                titulo_original=movie["titulo_original"],
+                imdb=movie["imdb"],
+                ano=movie["ano"],
+                # genero=movie["genero"],
+                tamanho_mb=movie["tamanho_mb"],
+                duracao_minutos=movie["duracao_minutos"],
+                qualidade=movie["qualidade"],
+                dublado=movie["dublado"],
+                sinopse=movie["sinopse"],
+                date_updated=movie["date_updated"],
+                link=movie["link"],
+            )
 
-            movie.date_updated = datetime.datetime.strptime(
-                movie.date_updated, "%Y-%m-%d %H:%M:%S"
+            list_of_genders = movie['genero'].split(" | ")
+
+            movie_entity.date_updated = datetime.datetime.strptime(
+                movie_entity.date_updated, "%Y-%m-%d %H:%M:%S"
             ).date()
             # check if movie already exists
             if sess.query(Movie).filter_by(
-                titulo_dublado=movie.titulo_dublado,
-                date_updated=movie.date_updated,
+                titulo_dublado=movie_entity.titulo_dublado,
+                date_updated=movie_entity.date_updated,
             ).first():
                 continue
             else:
-                sess.add(movie)
+                sess.add(movie_entity)
+
+                # add genders
+                for g in list_of_genders:
+                    gender = Gender(
+                        movie_id=sess.query(Movie).filter_by(
+                            titulo_dublado=movie_entity.titulo_dublado,
+                            date_updated=movie_entity.date_updated,
+                        ).first().id,
+                        gender=g.strip(),
+                    )
+                    sess.add(gender)
+
 
         sess.commit()
 
