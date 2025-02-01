e40ca8b93dea525f9b71c014ee8c39d893771aad
levyvix <levy.vix@gmail.com>
2025-02-01 20:26:33 -0300

    Add initial project files including JSON data and Docker setup

--- gratis_torrent/dag.py
+++ gratis_torrent/dag.py
@@ -1,19 +1,15 @@
-from prefect import flow, task
 from extract import main as extract
 from send_to_bq import main as send_to_bq
 
 
-@task
 def run_extract():
     extract()
 
 
-@task
 def run_send_to_bq():
     send_to_bq()
 
 
-@flow
 def run_dag():
     run_extract()
     run_send_to_bq()
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from scraper-filmes!")
+
+
+if __name__ == "__main__":
+    main()
